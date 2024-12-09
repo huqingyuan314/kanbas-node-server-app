10c7bc2317f55384e4e96aa5505bf765bdbe10bb
huqingyuan314 <hu.qingy@northeastern.edu>
2024-12-09 09:47:02 -0500

    update quiz schema

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -2,6 +2,7 @@ import * as dao from "./dao.js";
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
 import * as enrollmentsDao from "../Enrollments/dao.js";
+import * as quizzesDao from "../Quizzes/dao.js";
 
 export default function CourseRoutes(app) {
 
--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -2,12 +2,51 @@ import mongoose from "mongoose";
 
 const schema = new mongoose.Schema(
   {
-    name: String,
+    title: String,
     description: String,
-    points: Number,
-    dueDate: Date,
-    availableDate: Date,
-    availableUntilDate: Date,
+
+    quizType: {
+      type: String,
+      enum: ["GRADED-QUIZ", "PRACTICE-QUIZ", "GRADED-SURVEY", "UNGRADED-SURVEY"],
+      default: "GRADED-QUIZ",
+    },
+    points: { type: Number, default: 100 },
+    assignmentGroup: {
+      type: String,
+      enum: ["QUIZZES", "EXAMS", "ASSIGNMENTS", "PROJECT"],
+      default: "QUIZZES",
+    },
+    shuffleAnswers: { type: Boolean, default: true },
+    timeLimit: { type: Number, default: 20 },
+    multipleAttempts: { type: Boolean, default: false },
+    howManyAttempts: { type: Number, default: 1 },
+    showCorrectAnswers : {
+      type: String,
+      enum: ["IMMEDIATELY", "LATER", "..."],
+      default: "IMMEDIATELY",
+    },
+    accessCode: { type: String, default: "" },
+    oneQuestionAtATime : { type: Boolean, default: true },
+    webcamRequired: { type: Boolean, default: false },
+    lockQuestionsAfterAnswering : { type: Boolean, default: false },
+
+    dueDate: {
+      type: Date,
+      default: new Date('2024-12-31')
+    },
+    availableDate: {
+      type: Date,
+      default: new Date('2024-12-01')
+    },
+    availableUntilDate: {
+      type: Date,
+      default: new Date('2025-01-14')
+    },
+
+    numOfQuestions: { type: Number, default: 10 },
+
+    published: { type: Boolean, default: false},
+
     course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel" },
   },
   { collection: "quizzes" }
